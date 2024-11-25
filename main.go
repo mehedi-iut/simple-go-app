2c33f5d3e4cdd2acc564447d132ebdc0b72b6282
mehedi-iut <mahedihasan@iut-dhaka.edu>
2024-11-25 13:10:46 +0600

    modify file for testing

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 
 func main(){
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello Kubernetes</h1>"))
+		w.Write([]byte("<h1>Hello Kubernetes from cefalo</h1>"))
 	})
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello from Argo CD</h1>"))
+		w.Write([]byte("<h1>Hello from Argo CD from cefalo</h1>"))
 	})
 
 	log.Fatal(http.ListenAndServe(":9090", nil))
